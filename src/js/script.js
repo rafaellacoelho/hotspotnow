7096102186ad4657f319baca8c8881ff905c56b0
rafaellacoelho <rafaellacoelho@icloud.com>
2023-08-02 08:58:52 -0300

    Styling of the first part of the menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,194 +1,217 @@
 feather.replace()
 
 
-    let barChart = document.getElementById('barChart').getContext('2d');
-    let pizzaChart = document.getElementById('pizzaChart').getContext('2d');
-    let doughnutChart = document.getElementById('doughnutChart').getContext('2d');
+let barChart = document.getElementById('barChart').getContext('2d');
+let pizzaChart = document.getElementById('pizzaChart').getContext('2d');
+let doughnutChart = document.getElementById('doughnutChart').getContext('2d');
 
-    // image object
-    const image = new Image(12,12);
-    image.src = '../src/img/vector.svg'
+// image object
+const image = new Image(12, 12);
+image.src = '../src/img/vector.svg'
 
-    // BAR CHART
-    const xValues = [
-      "14/08",
-      "15/08",
-      "16/08",
-      "17/08",
-      "18/08",
-      "19/08",
-      "20/08",
-      "21/08",
-      "22/08",
-    ];
+// BAR CHART
+const xValues = [
+  "14/08",
+  "15/08",
+  "16/08",
+  "17/08",
+  "18/08",
+  "19/08",
+  "20/08",
+  "21/08",
+  "22/08",
+];
 
-    const accessNet = [40, 30, 23, 29, 33, 45, 42, 39, 27];
+const accessNet = [40, 30, 23, 29, 33, 45, 42, 39, 27];
 
-    let chart = new Chart(barChart, {
-      // The Type of Chart we want to create
-      type: "bar",
+let chart = new Chart(barChart, {
+  // The Type of Chart we want to create
+  type: "bar",
 
-      // The data for our dataset
-      data: {
-        labels: xValues,
-        datasets: [
-          {            
-            barPercentage: 0.6,
-            backgroundColor: "#D1D1F9",
-            hoverBackgroundColor: "#5A2357",
-            borderRadius: 10,
-            data: accessNet,
-          },
-        ],
+  // The data for our dataset
+  data: {
+    labels: xValues,
+    datasets: [{
+      barPercentage: 0.6,
+      backgroundColor: "#D1D1F9",
+      hoverBackgroundColor: "#5A2357",
+      borderRadius: 10,
+      data: accessNet,
+    }, ],
+  },
+
+  options: {
+    maintainAspectRatio: false,
+    animation: {
+      duration: 2000,
+    },
+    plugins: {
+      legend: {
+        display: false,
       },
-
-      options: {
-        maintainAspectRatio: false,
-        animation: {
-          duration: 2000,
+      tooltip: {
+        yAlign: 'bottom',
+        usePointStyle: true,
+        padding: {
+          x: 16,
+          y: 6
         },
-        plugins: {
-          legend: {
-            display: false,
+        callbacks: {
+          title: function () {
+            return '';
           },
-          tooltip: {
-            yAlign: 'bottom',
-            usePointStyle: true,
-            padding: {
-              x: 16,
-              y: 6
-            },
-            callbacks: {
-              title: function() {
-                  return '';
-              },
-              labelPointStyle: (context) => {
-                return {
-                  pointStyle: image,
-                }
-              }
-            },
-            
-          }
-        },
-        scales: {
-          x: {            
-            grid: {
-              display: false
-            },
-            border: {
-              display: false
-            },
-            ticks: {
-              color: '#4F4F4F',
-              font: {
-                family: "'Poppins', sans-serif",
-                size: 12,
-              }
+          labelPointStyle: (context) => {
+            return {
+              pointStyle: image,
             }
-          },
-          y: {
-            grid: {
-              display: false
-            },
-            border: {
-              display: false
-            },
-            min: 0
           }
-        }
-      }
-    });
-
-    
-
-    // Pizza Charts
-
-    yValues = [706, 135, 591, 284, 105, 110]
-
-    let PizzaChart = new Chart('pizzaChart', {
-      type: 'pie', 
-      data: {
-        labels: yValues, 
-        datasets: [{
-          data: yValues, 
-          backgroundColor: [
-            "#5A2357",
-            "#7B2869",
-            "#9D3C72",
-            "#C85C8E",
-            "#FFBABA",
-            "#FFE0E0"],
-        }],
-      },
-      options: {
-        maintainAspectRatio: false,
-        animation: {
-          duration: 2000,
-        },        
-        layout: {
-          padding: 22,
         },
-        plugins: {
-          tooltip: {
-            enabled: false,
-          },
-          labels: {
-            position: 'outside',
-            render: 'value',
-            textMargin: 6
-          },
-          legend: {
-            display: false
+
+      }
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false
+        },
+        border: {
+          display: false
+        },
+        ticks: {
+          color: '#4F4F4F',
+          font: {
+            family: "'Poppins', sans-serif",
+            size: 12,
           }
         }
-      },     
-    });
-
-    // Doughnut Chart
-    zValues = [167,120]
-
-    const doughnutLabel = {
-      id: 'doughnutLabel',
-      beforeDatasetsDraw(chart, args, pluginOptions) {
-        const {ctx, data} = chart;
-
-        ctx.save();
-        const xCoor = chart.getDatasetMeta(0).data[0].x;
-        const yCoor = chart.getDatasetMeta(0).data[0].y;
-
-        ctx.font = '30px Poppins';
-        ctx.fillStyle = '#383737';
-        ctx.textAlign = 'center';
-        ctx.textBaseline = 'middle'
-        ctx.fillText(zValues[0] + zValues[1], xCoor, yCoor - 15);
-        ctx.fillText('Total', xCoor, yCoor + 25);
-
+      },
+      y: {
+        grid: {
+          display: false
+        },
+        border: {
+          display: false
+        },
+        min: 0
       }
     }
+  }
+});
 
-    let DoughnutChart = new Chart('doughnutChart', {
-      type: 'doughnut', 
-      data: {
-        labels: zValues, 
-        datasets: [{
-          data: zValues, 
-          backgroundColor: [
-            "#5A2357",
-            "#9D3C72"],
-            cutout: '90',
-        }]
+
+
+// Pizza Charts
+
+yValues = [706, 135, 591, 284, 105, 110]
+
+let PizzaChart = new Chart('pizzaChart', {
+  type: 'pie',
+  data: {
+    labels: yValues,
+    datasets: [{
+      data: yValues,
+      backgroundColor: [
+        "#5A2357",
+        "#7B2869",
+        "#9D3C72",
+        "#C85C8E",
+        "#FFBABA",
+        "#FFE0E0"
+      ],
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    animation: {
+      duration: 2000,
+    },
+    layout: {
+      padding: 22,
+    },
+    plugins: {
+      tooltip: {
+        enabled: false,
       },
-      options: {
-        maintainAspectRatio: false,
-        animation: {
-          duration: 2000,
-        },
-        plugins: {
-          legend: {
-            display: false
-          }
-        }
+      labels: {
+        position: 'outside',
+        render: 'value',
+        textMargin: 6
       },
-      plugins: [doughnutLabel]
-    });
+      legend: {
+        display: false
+      }
+    }
+  },
+});
+
+// Doughnut Chart
+zValues = [167, 120]
+
+const doughnutLabel = {
+  id: 'doughnutLabel',
+  beforeDatasetsDraw(chart, args, pluginOptions) {
+    const {
+      ctx,
+      data
+    } = chart;
+
+    ctx.save();
+    const xCoor = chart.getDatasetMeta(0).data[0].x;
+    const yCoor = chart.getDatasetMeta(0).data[0].y;
+
+    ctx.font = '30px Poppins';
+    ctx.fillStyle = '#383737';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle'
+    ctx.fillText(zValues[0] + zValues[1], xCoor, yCoor - 15);
+    ctx.fillText('Total', xCoor, yCoor + 25);
+
+  }
+}
+
+let DoughnutChart = new Chart('doughnutChart', {
+  type: 'doughnut',
+  data: {
+    labels: zValues,
+    datasets: [{
+      data: zValues,
+      backgroundColor: [
+        "#5A2357",
+        "#9D3C72"
+      ],
+      cutout: '90',
+
+    }]
+  },
+  options: {
+    maintainAspectRatio: false,
+    animation: {
+      duration: 2000,
+    },
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
+  },
+  plugins: [doughnutLabel]
+});
+
+
+// MENU 
+
+var mini = true;
+
+function toggleSidebar() {
+  if (mini) {
+    console.log("opening sidebar");
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    this.mini = false;
+  } else {
+    console.log("closing sidebar");
+    document.getElementById("mySidebar").style.width = "85px";
+    document.getElementById("main").style.marginLeft = "85px";
+    this.mini = true;
+  }
+}
