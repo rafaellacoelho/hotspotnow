15df7c8b0a18021c80505e53651f59de35c11b3b
rafaellacoelho <rafaellacoelho@icloud.com>
2023-08-07 18:41:41 -0300

    User creation modal styling

--- src/js/script.js
+++ src/js/script.js
@@ -1,167 +1,156 @@
 feather.replace()
 
 
-    let barChart = document.getElementById('barChart').getContext('2d');
-    let pizzaChart = document.getElementById('pizzaChart').getContext('2d');
-    
-    // image object
-    const image = new Image(12,12);
-    image.src = '../src/img/vector.svg'
+let barChart = document.getElementById('barChart').getContext('2d');
+let pizzaChart = document.getElementById('pizzaChart').getContext('2d');
 
-    // BAR CHART
-    const xValues = [
-      "14/08",
-      "15/08",
-      "16/08",
-      "17/08",
-      "18/08",
-      "19/08",
-      "20/08",
-      "21/08",
-      "22/08",
-    ];
+// image object
+const image = new Image(12, 12);
+image.src = '../src/img/vector.svg'
 
-    const accessNet = [40, 30, 23, 29, 33, 45, 42, 39, 27];
+// BAR CHART
+const xValues = [
+  "14/08",
+  "15/08",
+  "16/08",
+  "17/08",
+  "18/08",
+  "19/08",
+  "20/08",
+  "21/08",
+  "22/08",
+];
 
-    let chart = new Chart(barChart, {
-      // The Type of Chart we want to create
-      type: "bar",
+const accessNet = [40, 30, 23, 29, 33, 45, 42, 39, 27];
 
-      // The data for our dataset
-      data: {
-        labels: xValues,
-        datasets: [
-          {            
-            barPercentage: 0.6,
-            backgroundColor: "#D1D1F9",
-            hoverBackgroundColor: "#5A2357",
-            borderRadius: 10,
-            data: accessNet,
-          },
-        ],
+let chart = new Chart(barChart, {
+  // The Type of Chart we want to create
+  type: "bar",
+
+  // The data for our dataset
+  data: {
+    labels: xValues,
+    datasets: [{
+      barPercentage: 0.6,
+      backgroundColor: "#D1D1F9",
+      hoverBackgroundColor: "#5A2357",
+      borderRadius: 10,
+      data: accessNet,
+    }, ],
+  },
+
+  options: {
+    maintainAspectRatio: false,
+    animation: {
+      duration: 2000,
+    },
+    plugins: {
+      legend: {
+        display: false,
       },
-
-      options: {
-        maintainAspectRatio: false,
-        animation: {
-          duration: 2000,
+      tooltip: {
+        yAlign: 'bottom',
+        usePointStyle: true,
+        padding: {
+          x: 16,
+          y: 6
         },
-        plugins: {
-          legend: {
-            display: false,
+        callbacks: {
+          title: function () {
+            return '';
           },
-          tooltip: {
-            yAlign: 'bottom',
-            usePointStyle: true,
-            padding: {
-              x: 16,
-              y: 6
-            },
-            callbacks: {
-              title: function() {
-                  return '';
-              },
-              labelPointStyle: (context) => {
-                return {
-                  pointStyle: image,
-                }
-              }
-            },
-            
-          }
-        },
-        scales: {
-          x: {            
-            grid: {
-              display: false
-            },
-            border: {
-              display: false
-            },
-            ticks: {
-              color: '#4F4F4F',
-              font: {
-                family: "'Poppins', sans-serif",
-                size: 12,
-              }
+          labelPointStyle: (context) => {
+            return {
+              pointStyle: image,
             }
-          },
-          y: {
-            grid: {
-              display: false
-            },
-            border: {
-              display: false
-            },
-            min: 0
           }
-        }
-      }
-    });
-
-    
-
-    // Pizza Charts
-
-    yValues = [706, 135, 591, 284, 105, 110]
-
-    let PizzaChart = new Chart('pizzaChart', {
-      type: 'pie', 
-      data: {
-        labels: yValues, 
-        datasets: [{
-          data: yValues, 
-          backgroundColor: [
-            "#5A2357",
-            "#7B2869",
-            "#9D3C72",
-            "#C85C8E",
-            "#FFBABA",
-            "#FFE0E0"],
-        }],
-      },
-      options: {
-        maintainAspectRatio: false,
-        animation: {
-          duration: 2000,
-        },        
-        layout: {
-          padding: 22,
         },
-        plugins: {
-          tooltip: {
-            enabled: false,
-          },
-          labels: {
-            position: 'outside',
-            render: 'value',
-            textMargin: 6
-          },
-          legend: {
-            display: false
+
+      }
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false
+        },
+        border: {
+          display: false
+        },
+        ticks: {
+          color: '#4F4F4F',
+          font: {
+            family: "'Poppins', sans-serif",
+            size: 12,
           }
         }
-      },     
-    });
-
-
-    // MENU 
-
-    var mini = true;
-
-function toggleSidebar() {
-  if (mini) {
-    console.log("opening sidebar");
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    this.mini = false;
-  } else {
-    console.log("closing sidebar");
-    document.getElementById("mySidebar").style.width = "85px";
-    document.getElementById("main").style.marginLeft = "85px";
-    this.mini = true;
+      },
+      y: {
+        grid: {
+          display: false
+        },
+        border: {
+          display: false
+        },
+        min: 0
+      }
+    }
   }
-}
+});
+
+
+
+// Pizza Charts
+
+connection = ['Hotel Bristol 1° Andar', 'Hotel Bristol 3° Andar', 'Hotel Bristol Refeitorio', 'Hotel Bristol Salão', 'Hotel Bristol Recepção', 'Hotel Bristol 2° Andar']
+yValues = [706, 135, 591, 284, 105, 110]
+
+let PizzaChart = new Chart('pizzaChart', {
+  type: 'pie',
+  data: {
+    labels: connection,
+    datasets: [{
+      data: yValues,
+      backgroundColor: [
+        "#5A2357",
+        "#7B2869",
+        "#9D3C72",
+        "#C85C8E",
+        "#FFBABA",
+        "#FFE0E0"
+      ],
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    animation: {
+      duration: 2000,
+    },
+    layout: {
+      padding: 22,
+    },
+    plugins: {
+      tooltip: {
+        backgroundColor: '#FBFAFF',
+        titleColor: '#000',
+        cornerRadius: 12,
+        caretSize: 0,
+        callbacks: {
+          label: () => {
+            return ''
+          },
+        }
+      },
+      labels: {
+        position: 'outside',
+        render: 'value',
+        textMargin: 3
+      },
+      legend: {
+        display: false
+      }
+    }
+  },
+});
 
 // Drop 
 
@@ -192,3 +181,20 @@ document.addEventListener("DOMContentLoaded", function(){
     }); // addEventListener
   }) // forEach
 }); 
+
+
+// MENU 
+
+var mini = true;
+
+function toggleSidebar() {
+  if (mini) {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    this.mini = false;
+  } else {
+    document.getElementById("mySidebar").style.width = "85px";
+    document.getElementById("main").style.marginLeft = "85px";
+    this.mini = true;
+  }
+}
